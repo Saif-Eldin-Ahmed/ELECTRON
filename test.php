047d9d4cb4f173c4768c91cea7b5a83664cbf2c9
Saif-Eldin-Ahmed <saif.hamada361@gmail.com>
2026-06-20 15:10:25 +0300

    Added a test file

---
+++ test.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "This is PHP";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TEST</title>
+</head>
+
+<body>
+    <h1>This is HTML</h1>
+</body>
+
+</html>
