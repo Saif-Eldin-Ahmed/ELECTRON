6a0bcb2900fa7c641f2a2695c8df3bbb57f23dc1
Saif-Eldin-Ahmed <saif.hamada361@gmail.com>
2026-06-20 15:13:50 +0300

    Test included header block in test,php

---
+++ test.php
@@ -0,0 +1,20 @@
+<?php
+
+echo "This is PHP";
+include "includes/header.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TEST</title>
+</head>
+
+<body>
+    <h1>This is HTML</h1>
+</body>
+
+</html>
