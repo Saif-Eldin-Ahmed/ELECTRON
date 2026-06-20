745f0b2fbe4531655c90e9cf7666b45d00709f80
Saif-Eldin-Ahmed <saif.hamada361@gmail.com>
2026-06-20 20:31:50 +0300

    Attemp 4

--- dashboard/add-product.php
+++ dashboard/add-product.php
@@ -6,7 +6,6 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 if (!isset($_SESSION['id']) || $_SESSION['role'] !== 'admin') {
-    header("Location: ../index.php");
     exit;
 }
 
