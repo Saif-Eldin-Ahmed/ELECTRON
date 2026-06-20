7a9654597530b282580822b76da7d3fd0a666630
Saif-Eldin-Ahmed <saif.hamada361@gmail.com>
2026-06-20 20:09:13 +0300

    Attemp 1

--- dashboard/index.php
+++ dashboard/index.php
@@ -2,14 +2,19 @@
 // ============================================================
 //  dashboard/index.php — Admin Dashboard
 // ============================================================
-session_start();
+
+// Adminestrator can only access this page
+
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 if (!isset($_SESSION['id']) || $_SESSION['role'] !== 'admin') {
-    header("Location: /index.php");
+    header("Location: ../index.php");
     exit;
 }
 
 $acc = true; // Exclude front-end navbar
-require_once '/includes/config.php';
+require_once '../includes/config.php';
 
 try {
     $pdo = getDBConnection();
